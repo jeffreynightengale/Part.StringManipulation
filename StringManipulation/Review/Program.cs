40861a0db23eeaaabbcde4ca43b60b32196059d7
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-03-09 18:17:42 -0600

    Added review

---
+++ StringManipulation/Review/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {             //  012345678901  
+            /*const string msg = "Hello World!";
+
+            int indexOfFirstW = msg.IndexOf('H');
+            
+            Console.WriteLine(msg);
+            Console.WriteLine(indexOfFirstW);*/
+
+            string number = "55s";
+            int realNum; // = Convert.ToInt32(number);
+            //bool b = int.TryParse(number, out realNum);
+           
+            if (/*b*/int.TryParse(number, out realNum) == true)
+            {
+            Console.WriteLine("Good number");
+            }
+            else
+            {
+                Console.WriteLine("Bad number");
+            }
+            Console.WriteLine(number + 5);
+
+            
+        }
+    }
+}
--- StringManipulation/StringManipulation/Program.cs
+++ StringManipulation/StringManipulation/Program.cs
@@ -6,7 +6,7 @@ namespace StringManipulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.");
+            Console.WriteLine("Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.");
             string s1 = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
 
             Console.WriteLine($"What word would you like to search for?");
